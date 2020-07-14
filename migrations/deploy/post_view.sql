ec92cbcff3929d804bbee102e0f87908e0579dfd
Samuel Beaulieu <sbeauli2@unb.ca>
2020-07-14 14:55:08 -0300

    Reduced number of columns shown in post_view

--- migrations/deploy/post_view.sql
+++ migrations/deploy/post_view.sql
@@ -8,8 +8,27 @@ ALTER TABLE stackdump.posts ADD COLUMN deleted BOOLEAN DEFAULT false;
 UPDATE stackdump.posts SET deleted = false;
 
 CREATE VIEW stackdump.post_view AS
-    (SELECT * FROM stackdump.posts WHERE deleted = false);
-
-
+    (SELECT id,
+            postTypeId,
+            acceptedAnswerId,
+            parentId,
+            creationDate,
+            score,
+            viewCount,
+            body,
+            ownerUserId,
+            ownerDisplayName,
+            lastEditorUserId,
+            lastEditDate,
+            lastActivityDate,
+            title,
+            tags,
+            answerCount,
+            commentCount,
+            favoriteCount,
+            closedDate,
+            communityOwnedDate,
+            contentLicense
+        FROM stackdump.posts WHERE deleted = false);
 
 COMMIT;
