ffc59e04e0c4e6e193158c571deb8918992e383b
Samuel Beaulieu <sbeauli2@unb.ca>
2020-08-05 14:17:55 -0300

    Added post update trigger

--- migrations/deploy/post_triggers.sql
+++ migrations/deploy/post_triggers.sql
@@ -60,6 +60,46 @@ CREATE TRIGGER post_on_delete INSTEAD OF DELETE
     FOR EACH ROW
     EXECUTE PROCEDURE stackdump_private.post_delete_trigger();
 
+CREATE FUNCTION stackdump_private.post_update_trigger() RETURNS TRIGGER AS $$
+    DECLARE
+        i INTEGER;
+    BEGIN
+        SET ROLE user_super;
+        IF new.title != old.title THEN
+            INSERT INTO stackdump.postHistory(postHistoryTypeId, postId, revisionGUID, userId, text)
+                VALUES (4, new.id, extensions.uuid_generate_v4(), new.ownerUserId, new.title);
+        END IF;
+        IF new.body != old.body THEN
+            INSERT INTO stackdump.postHistory(postHistoryTypeId, postId, revisionGUID, userId, text)
+                VALUES (5, new.id, extensions.uuid_generate_v4(), new.ownerUserId, new.body);
+        END IF;
+        IF new.tags != old.tags THEN
+            IF array_upper(old.tags, 1) >  1 THEN 
+                FOR i IN 1 .. array_upper(old.tags, 1)
+                LOOP
+                    UPDATE stackdump.tags SET count = count - 1 WHERE tagName = old.tags[i];
+                END LOOP;
+            END IF;
+            IF array_upper(new.tags, 1) >  1 THEN 
+                FOR i IN 1 .. array_upper(new.tags, 1)
+                LOOP
+                    UPDATE stackdump.tags SET count = count + 1 WHERE tagName = new.tags[i];
+                END LOOP;
+                INSERT INTO stackdump.postHistory(postHistoryTypeId, postId, revisionGUID, userId, text)
+                    VALUES (6, new.id, extensions.uuid_generate_v4(), new.ownerUserId, new.tags);
+            END IF;
+        END IF;
+        RESET ROLE;
+        RETURN new;
+    END;
+$$ LANGUAGE PLPGSQL;
+
+CREATE TRIGGER post_on_update AFTER UPDATE
+    ON stackdump.posts
+    FOR EACH ROW
+    EXECUTE PROCEDURE stackdump_private.post_update_trigger();
+    
+
 CREATE OR REPLACE FUNCTION stackdump_private.check_parent() RETURNS trigger AS $$
 DECLARE
     pid INTEGER;
