d561798bc675b2c9af7cc348c07eb137a82759b8
Samuel Beaulieu <sbeauli2@unb.ca>
2020-08-05 14:39:37 -0300

    Changed how GUIDs for postHistory are added.

--- migrations/deploy/post_triggers.sql
+++ migrations/deploy/post_triggers.sql
@@ -9,16 +9,17 @@ BEGIN;
 CREATE FUNCTION stackdump_private.post_create_trigger() RETURNS TRIGGER AS $$
     DECLARE
         i INTEGER;
+        newGUID TEXT := extensions.uuid_generate_v4();
     BEGIN
         SET ROLE user_super;
         UPDATE stackdump.posts SET answerCount = answerCount + 1
             WHERE posts.id = new.parentId;
         INSERT INTO stackdump.postHistory(postHistoryTypeId, postId, revisionGUID, userId, text)
-            VALUES (1, new.id, extensions.uuid_generate_v4(), new.ownerUserId, new.title);
+            VALUES (1, new.id, newGUID, new.ownerUserId, new.title);
         INSERT INTO stackdump.postHistory(postHistoryTypeId, postId, revisionGUID, userId, text)
-            VALUES (2, new.id, extensions.uuid_generate_v4(), new.ownerUserId, new.body);
+            VALUES (2, new.id, newGUID, new.ownerUserId, new.body);
         INSERT INTO stackdump.postHistory(postHistoryTypeId, postId, revisionGUID, userId, text)
-            VALUES (3, new.id, extensions.uuid_generate_v4(), new.ownerUserId, ARRAY_TO_STRING(new.tags, ' '));
+            VALUES (3, new.id, newGUID, new.ownerUserId, ARRAY_TO_STRING(new.tags, ' '));
         IF array_upper(new.tags, 1) >  1 THEN 
             FOR i IN 1 .. array_upper(new.tags, 1)
             LOOP
@@ -63,15 +64,16 @@ CREATE TRIGGER post_on_delete INSTEAD OF DELETE
 CREATE FUNCTION stackdump_private.post_update_trigger() RETURNS TRIGGER AS $$
     DECLARE
         i INTEGER;
+        newGUID TEXT := extensions.uuid_generate_v4();
     BEGIN
         SET ROLE user_super;
         IF new.title != old.title THEN
             INSERT INTO stackdump.postHistory(postHistoryTypeId, postId, revisionGUID, userId, text)
-                VALUES (4, new.id, extensions.uuid_generate_v4(), new.ownerUserId, new.title);
+                VALUES (4, new.id, newGUID, new.ownerUserId, new.title);
         END IF;
         IF new.body != old.body THEN
             INSERT INTO stackdump.postHistory(postHistoryTypeId, postId, revisionGUID, userId, text)
-                VALUES (5, new.id, extensions.uuid_generate_v4(), new.ownerUserId, new.body);
+                VALUES (5, new.id, newGUID, new.ownerUserId, new.body);
         END IF;
         IF new.tags != old.tags THEN
             IF array_upper(old.tags, 1) >  1 THEN 
@@ -86,7 +88,7 @@ CREATE FUNCTION stackdump_private.post_update_trigger() RETURNS TRIGGER AS $$
                     UPDATE stackdump.tags SET count = count + 1 WHERE tagName = new.tags[i];
                 END LOOP;
                 INSERT INTO stackdump.postHistory(postHistoryTypeId, postId, revisionGUID, userId, text)
-                    VALUES (6, new.id, extensions.uuid_generate_v4(), new.ownerUserId, new.tags);
+                    VALUES (6, new.id, newGUID, new.ownerUserId, new.tags);
             END IF;
         END IF;
         RESET ROLE;
