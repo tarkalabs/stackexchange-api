2ed3f3e049bbb98dd7b36d0804d5731ef1b1099c
Samuel Beaulieu <sbeauli2@unb.ca>
2020-08-12 14:59:32 -0300

    Updated Sqitch verify statements for comments and comment smart comments.

--- migrations/verify/cmt_rls_comments.sql
+++ migrations/verify/cmt_rls_comments.sql
@@ -2,9 +2,21 @@
 
 BEGIN;
 
-SELECT 1 FROM pg_policies WHERE policyname='policy_insert_comments';
-SELECT 1 FROM pg_policies WHERE policyname='policy_delete_comments';
-SELECT 1 FROM pg_policies WHERE policyname='policy_update_comments';
-SELECT 1 FROM pg_policies WHERE policyname='policy_select_comments';
+DO $$
+    BEGIN
+        ASSERT LENGTH((SELECT col_description((SELECT 'stackdump.comments'::regclass::oid), 1)))::boolean;
+        ASSERT LENGTH((SELECT col_description((SELECT 'stackdump.comments'::regclass::oid), 3)))::boolean;
+        ASSERT LENGTH((SELECT col_description((SELECT 'stackdump.comments'::regclass::oid), 5)))::boolean;
+
+        ASSERT (SELECT relrowsecurity FROM pg_class WHERE oid = (SELECT 'stackdump.comments'::regclass::oid));
+
+        ASSERT (SELECT 1 FROM pg_policies WHERE policyname='policy_insert_comments');
+        ASSERT (SELECT 1 FROM pg_policies WHERE policyname='policy_delete_comments');
+        ASSERT (SELECT 1 FROM pg_policies WHERE policyname='policy_update_comments');
+        ASSERT (SELECT 1 FROM pg_policies WHERE policyname='policy_select_comments');
+    END;
+$$ LANGUAGE PLPGSQL;
+
+
 
 ROLLBACK;
--- migrations/verify/comments.sql
+++ migrations/verify/comments.sql
@@ -6,4 +6,8 @@ SELECT id, postId, score
     FROM stackdump.comments
     WHERE FALSE;
 
+SELECT 'stackdump.comments_id_idx'::regclass;
+SELECT 'stackdump.comments_postId_idx'::regclass;
+SELECT 'stackdump.comments_userId_idx'::regclass;
+
 ROLLBACK;
