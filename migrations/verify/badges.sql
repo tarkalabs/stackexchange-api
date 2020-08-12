cd6aa2542402ea2c947b23d871ddacb5aa2398fc
Samuel Beaulieu <sbeauli2@unb.ca>
2020-08-12 14:42:42 -0300

    Updated Sqitch verify statements for badges and badge smart comments.

--- migrations/verify/badges.sql
+++ migrations/verify/badges.sql
@@ -6,4 +6,7 @@ SELECT id, userId, name
     FROM stackdump.badges
     WHERE FALSE;
 
+SELECT 'stackdump.badges_id_idx'::regclass;
+SELECT 'stackdump.badges_userId_idx'::regclass;
+
 ROLLBACK;
--- migrations/verify/badges_rls_comments.sql
+++ migrations/verify/badges_rls_comments.sql
@@ -2,6 +2,12 @@
 
 BEGIN;
 
-SELECT 1 + 1;
+DO $$
+    BEGIN
+        ASSERT LENGTH((SELECT col_description((SELECT 'stackdump.badges'::regclass::oid), 1)))::boolean;
+        ASSERT LENGTH((SELECT col_description((SELECT 'stackdump.badges'::regclass::oid), 2)))::boolean;
+        ASSERT LENGTH((SELECT col_description((SELECT 'stackdump.badges'::regclass::oid), 4)))::boolean;
+    END;
+$$ LANGUAGE PLPGSQL;
 
 ROLLBACK;
