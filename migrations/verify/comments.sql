0770f39b60166b68d01f19956df0ecfa36d77a06
Samuel Beaulieu <sbeauli2@unb.ca>
2020-08-13 12:40:14 -0300

    Rewrote verify scripts for comments, tags, postHistoryTypes.

--- migrations/verify/comments.sql
+++ migrations/verify/comments.sql
@@ -2,12 +2,13 @@
 
 BEGIN;
 
-SELECT id, postId, score
-    FROM stackdump.comments
-    WHERE FALSE;
-
-SELECT 'stackdump.comments_id_idx'::regclass;
-SELECT 'stackdump.comments_postId_idx'::regclass;
-SELECT 'stackdump.comments_userId_idx'::regclass;
+DO $$
+    BEGIN
+        ASSERT(SELECT 1 FROM pg_catalog.pg_tables WHERE tablename = 'comments'), 'Comments table was not created.';
+        ASSERT(SELECT 1 FROM pg_indexes WHERE indexname = 'comments_id_idx'), 'Comments id index was not created.';
+        ASSERT(SELECT 1 FROM pg_indexes WHERE indexname = 'comments_postid_idx'), 'Comments postId index was not created.';
+        ASSERT(SELECT 1 FROM pg_indexes WHERE indexname = 'comments_userid_idx'), 'Comments userId index was not created.';
+    END;
+$$ LANGUAGE PLPGSQL;
 
 ROLLBACK;
--- migrations/verify/postHistoryTypes.sql
+++ migrations/verify/postHistoryTypes.sql
@@ -2,8 +2,11 @@
 
 BEGIN;
 
-SELECT id, name
-    FROM stackdump.postHistoryTypes
-    WHERE FALSE;
+DO $$
+    BEGIN
+        ASSERT(SELECT 1 FROM pg_catalog.pg_tables WHERE tablename = 'posthistorytypes'), 'PostHistoryTypes table was not created.';
+        ASSERT(SELECT COUNT(*) FROM stackdump.postHistoryTypes) = 40, 'Rows not added to PostHistoryTypes table.';
+    END;
+$$ LANGUAGE PLPGSQL;
 
 ROLLBACK;
