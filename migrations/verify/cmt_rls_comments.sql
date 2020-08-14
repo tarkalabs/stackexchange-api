eed676f2e929d09c76563e4534e11616a18ed093
Samuel Beaulieu <sbeauli2@unb.ca>
2020-08-14 13:36:13 -0300

    Rewrote verify scripts for user_rls_comments, grants_user, cmt_rls_comments, grants_comments.

--- migrations/verify/cmt_rls_comments.sql
+++ migrations/verify/cmt_rls_comments.sql
@@ -4,16 +4,16 @@ BEGIN;
 
 DO $$
     BEGIN
-        ASSERT LENGTH((SELECT col_description((SELECT 'stackdump.comments'::regclass::oid), 1)))::boolean;
-        ASSERT LENGTH((SELECT col_description((SELECT 'stackdump.comments'::regclass::oid), 3)))::boolean;
-        ASSERT LENGTH((SELECT col_description((SELECT 'stackdump.comments'::regclass::oid), 5)))::boolean;
+        ASSERT LENGTH((SELECT col_description((SELECT 'stackdump.comments'::regclass::oid), 1)))::boolean, 'Comments id smart comment not created.';
+        ASSERT LENGTH((SELECT col_description((SELECT 'stackdump.comments'::regclass::oid), 3)))::boolean, 'Comments score smart comment not created.';
+        ASSERT LENGTH((SELECT col_description((SELECT 'stackdump.comments'::regclass::oid), 5)))::boolean, 'Comments creationDate smart comment not created.';
 
-        ASSERT (SELECT relrowsecurity FROM pg_class WHERE oid = (SELECT 'stackdump.comments'::regclass::oid));
+        ASSERT (SELECT relrowsecurity FROM pg_class WHERE oid = (SELECT 'stackdump.comments'::regclass::oid)), 'Row level security not activated on comments table.';
 
-        ASSERT (SELECT 1 FROM pg_policies WHERE policyname='policy_insert_comments');
-        ASSERT (SELECT 1 FROM pg_policies WHERE policyname='policy_delete_comments');
-        ASSERT (SELECT 1 FROM pg_policies WHERE policyname='policy_update_comments');
-        ASSERT (SELECT 1 FROM pg_policies WHERE policyname='policy_select_comments');
+        ASSERT (SELECT 1 FROM pg_policies WHERE policyname='policy_insert_comments'), 'Policy_insert_comments policy not created.';
+        ASSERT (SELECT 1 FROM pg_policies WHERE policyname='policy_delete_comments'), 'Policy_delete_comments policy not created.';
+        ASSERT (SELECT 1 FROM pg_policies WHERE policyname='policy_update_comments'), 'Policy_update_comments policy not created.';
+        ASSERT (SELECT 1 FROM pg_policies WHERE policyname='policy_select_comments'), 'Policy_select_comments policy not created.';
     END;
 $$ LANGUAGE PLPGSQL;
 
--- migrations/verify/grants_user.sql
+++ migrations/verify/grants_user.sql
@@ -2,11 +2,15 @@
 
 BEGIN;
 
-SELECT has_sequence_privilege('user_reg','stackdump.users_id_seq','usage');
-SELECT has_sequence_privilege('user_reg','stackdump.users_id_seq','select');
-SELECT has_table_privilege('user_reg','stackdump.users','select');
-SELECT has_table_privilege('user_reg','stackdump.users','update');
-SELECT has_table_privilege('user_reg','stackdump.users','delete');
-SELECT has_table_privilege('user_anon','stackdump.users','select');
+DO $$
+    BEGIN
+        ASSERT(SELECT has_table_privilege('user_reg','stackdump.users','select')), 'User_reg was not given permission to select users.';
+        ASSERT(SELECT has_table_privilege('user_reg','stackdump.users','update')), 'User_reg was not given permission to update users.';
+        ASSERT(SELECT has_table_privilege('user_reg','stackdump.users','delete')), 'User_reg was not given permission to delete users.';
+        ASSERT(SELECT has_table_privilege('user_anon','stackdump.users','select')), 'User_anon was not given permission to select users.';
+        ASSERT(SELECT 1 FROM information_schema.usage_privileges WHERE grantee='user_reg' AND object_name='users_id_seq'), 'User_reg was not granted usage of sequence users_id_seq.';
+        ASSERT(SELECT array_to_string((SELECT relacl FROM pg_class WHERE relname='users_id_seq'), ',') ~ '.*user_reg=r.*'), 'User_reg was not granted select privileges on sequence users_id_seq';
+    END;
+$$ LANGUAGE PLPGSQL;
 
 ROLLBACK;
